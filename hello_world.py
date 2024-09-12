1a66551692aa1122efe46824ba66a9f2ffb5edc3
Heemin2be <heemin7904@gmail.com>
2024-09-12 17:35:24 +0900

    ver_2

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,10 @@
 def print_hello():
     print("Hello World!!")
 
+def print_KIT():
+    print("kumoh National Institution of Technolohy.")
+
 if __name__ == '__main__':
-    print_hello()
+    print_KIT()
+
+
